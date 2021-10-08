672189905a0726960c41d6254c312feb12453c62
Ryan Haire <rwhaire@hotmail.com>
2021-10-08 10:04:38 -0400

    Finished sign up page

--- AssignmentOne/Controllers/HomeController.cs
+++ AssignmentOne/Controllers/HomeController.cs
@@ -11,7 +11,37 @@ namespace AssignmentOne.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Index(User user)
+        {
+            if(ModelState.IsValid) {
+                var found = Repository.Users.Where(u => u.Email.ToLower().Equals(user.Email.ToLower()));
+
+                // turn found user into list and if there is a user return the view 
+                if(found.ToList().Count() > 0) 
+                {
+                    ViewBag.UserFound = true;
+                    return View();
+                } 
+                else 
+                {
+                    Repository.AddUser(user);
+                    return View("Login");
+                }
+            } 
+            else 
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Login() 
         {
             return View();
         }
--- AssignmentOne/Models/User.cs
+++ AssignmentOne/Models/User.cs
@@ -1,12 +1,29 @@
+using System.ComponentModel.DataAnnotations;
 namespace AssignmentOne.Models
 {
     public class User
     {
+        [Required(ErrorMessage="Please enter your first name")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage="Please enter your last name")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage="Please enter your email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage="Please enter your phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", 
+            ErrorMessage = "Phone Number should be in format 999-999-9999")]
+
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage="Please enter a password")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage="Please confirm your password")]
         public string ConfirmPassword{ get; set; }
     }
 }
